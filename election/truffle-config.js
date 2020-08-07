b016f4a1b3ffe67ee1d1426e6d9657c041f100f2
Roh Geonho <roh517@naver.com>
2020-08-07 11:40:45 +0900

    dapp

---
+++ election/truffle-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    },
+    compilers: {
+      version: "0.4.25"
+    }
+  }
+};
